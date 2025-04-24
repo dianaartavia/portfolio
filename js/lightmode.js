99e6cf845577e1a5b0cfd0a1e3f9fb90943f31c6
Diana <94877191+dianaartavia@users.noreply.github.com>
2025-04-23 23:01:24 -0600

    Update lightmode.js

--- js/lightmode.js
+++ js/lightmode.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const imgDiv = document.querySelector('.my-img');
     imgDiv.addEventListener('click', function () {
+       void imgDiv.offsetWidth;
         imgDiv.classList.toggle('active');
-        setTimeout(() => {
-            document.body.classList.toggle('light-mode');
-        }, 50);
+        document.body.classList.toggle('light-mode');
     });
 });
